94b00d6bcf7d4d901280c9ba43603c910d755535
Eldo M <eldoma@gmail.com>
2025-05-02 14:24:54 +0700

    Update consumption_app.py with new markdown and plot

--- consumption_app.py
+++ consumption_app.py
@@ -41,15 +41,17 @@ https://data.gov.sg/datasets/d_d57aee293789d31a8cb4097ad50e78cb/view
 📁 Official data from [data.gov.sg](https://data.gov.sg). Update Frequency: Monthly
 ---
 Key takeaways and notable trends: Singaporeans 
-- Consume more Edible Oil, Tea, Cocoa, Spices
+## Consume more Edible Oil, Tea, Cocoa, Spices
             
 Edible oils (Fixed Vegetable Fats & Oils) show volatile trends with large spikes and drops, but since 2010 the average monthly consumption appears to have stabilized at higher levels than pre-2000, indicating moderate to increased usage over time.
 
 Coffee, Tea, Cocoa, Spices & Manufactures show a clear upward trend, especially after 2010, with a strong spike in recent years — strongly suggesting growing consumption habits among Singaporeans for these products.
 
-- Singaporeans (both individuals and industries) are rapidly phasing out paper, aligning with digital and environmental trends.          
+## Singaporeans (both individuals and industries) are rapidly phasing out paper
 
-- Will be consuming less on Beverages
+Aligning with digital and environmental trends.          
+
+## Will be consuming less on Beverages
 
 The chart suggests potential stagnation or even decline in Singapore’s domestic beverage consumption, especially in recent years.
             
@@ -65,11 +67,11 @@ Shift to healthier or alternative drinks
 Export-driven beverage trade (less local use)
 So yes, unless corrected, this data may imply lower domestic beverage demand, or at least more erratic patterns going forward.
             
-- Stable Clothing consumption
+## Stable Clothing consumption
             
 The Singaporean net domestic consumption is modest and stable, but masked by high re-export flows and global supply chain roles.
             
-- Consuming a bit more Fish & Seafood:
+## Consuming a bit more Fish & Seafood:
             
 The chart strongly suggests that Singaporeans have been consuming more fish and seafood over time.
             
